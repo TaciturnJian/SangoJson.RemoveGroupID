22495e9f5853fcbdc1a85da178c4ac0d45ef2527
TaciturnJian <letusgoone@outlook.com>
2023-12-11 22:38:11 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+
+foreach (var file in Directory.GetFiles(@"C:\Users\Sango\Downloads\duff3\test"))
+{
+    var builder = new StringBuilder();
+    foreach (var line in File.ReadAllLines(file))
+    {
+        if (line.Contains("group_id")) continue;
+        builder.AppendLine(line);
+    }
+    File.WriteAllText(file, builder.ToString());
+}
